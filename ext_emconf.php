1d41b07251372c637462a463c960ac6ca73cd216
Armin Vieweg <armin@v.ieweg.de>
2022-08-01 18:16:10 +0200

    [TASK] Add extension-key to composer.json
    And adjust PHP requirement in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.0.0',
     'constraints' => [
         'depends' => [
-            'php' => '7.4.0-8.0.99',
+            'php' => '7.4.0-8.1.99',
             'typo3' => '10.4.6-11.5.99'
         ],
         'conflicts' => [],
